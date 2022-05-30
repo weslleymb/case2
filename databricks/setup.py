31450d1e4f73d589d34af3e382f1435d558b1b76
weslleymb <weslleymb>
2022-05-30 21:28:07 +0000

    Notebook de configuracao dos mount point
    Integracao com o Storage Account

---
+++ databricks/setup.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# DBTITLE 1,Mount Raw
+try:
+  dbutils.fs.mount(
+    source = "wasbs://raw@dlscase2deveastus.blob.core.windows.net",
+    mount_point = "/mnt/raw",
+    extra_configs = {"fs.azure.account.key.dlscase2deveastus.blob.core.windows.net":dbutils.secrets.get(scope = "dbw-kv-scope", key = "storageaccount-key")})
+except:
+  pass
+
+# COMMAND ----------
+
+# DBTITLE 1,Mount Bronze
+try:
+  dbutils.fs.mount(
+    source = "wasbs://bronze@dlscase2deveastus.blob.core.windows.net",
+    mount_point = "/mnt/bronze",
+    extra_configs = {"fs.azure.account.key.dlscase2deveastus.blob.core.windows.net":dbutils.secrets.get(scope = "dbw-kv-scope", key = "storageaccount-key")})
+except:
+  pass
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("/mnt/"))
